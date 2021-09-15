d695fa5a74cffaa31ca25b88dc08897e150e56b6
uche-okoroafor <ucheokoroafo01@gmil.com>
2021-09-15 22:55:11 +0400

    BE: Profile Model #7
    created a  profile model

---
+++ server/models/UserProfile.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const reqString = {
+  type: 'string',
+  required: true
+}
+
+const noReqString = {
+  type: 'string',
+  required: false
+}
+
+const userProfile = new mongoose.Schema(
+  {
+    firstName: reqString,
+    lastName: reqString,
+    userImage: reqString,
+    userId: reqString,
+    nickName: noReqString,
+    dateOfBirth: { type: Date, default: Date.now },
+    education: noReqString,
+    occupation: noReqString,
+    hobbies: noReqString,
+    postCode: noReqString,
+    address: noReqString,
+    city: noReqString,
+    country: noReqString,
+    online: {
+      type: 'boolean',
+      required: true
+    },
+    about: reqString
+  },
+  { timestamps: true }
+)
+
+const UserProfile = mongoose.model('UsersProfile', userProfile)
+module.exports = UserProfile
