e37a05b6e24fd1d886a9f09cb8cfdc9f0fca0f49
uche-okoroafor <ucheokoroafo01@gmil.com>
2021-09-15 23:16:27 +0400

    Update UserProfile.js

--- server/models/UserProfile.js
+++ server/models/UserProfile.js
@@ -14,7 +14,6 @@ const userProfile = new mongoose.Schema(
     firstName: reqString,
     lastName: reqString,
     userImage: reqString,
-    userId: reqString,
     nickName: noReqString,
     dateOfBirth: { type: Date, default: Date.now },
     education: noReqString,
