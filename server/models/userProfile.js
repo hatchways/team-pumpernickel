a5fdacea79693914fa24b0df860c2b5ec2550c9d
uche-okoroafor <ucheokoroafo01@gmil.com>
2021-09-22 23:43:36 +0400

    Update userProfile.js

--- server/models/userProfile.js
+++ server/models/userProfile.js
@@ -1,62 +1,61 @@
 const mongoose = require('mongoose')
 
-const userProfile = new mongoose.Schema({
+const userProfile = new mongoose.Schema(
+  {
     firstName: {
-        type: 'string',
-        required: false
+      type: 'string',
+      required: false
     },
     lastName: {
-        type: 'string',
-        required: false
+      type: 'string',
+      required: false
     },
     gender: {
-        type: 'string',
-        required: false
+      type: 'string',
+      required: false
     },
     birthDay: {
-        type: Date,
-        default: null
+      type: Date,
+      default: null
     },
     email: {
-        type: 'string',
-        required: false
+      type: 'string',
+      required: false
     },
     phoneNumber: {
-        type: 'string',
-        required: false
+      type: 'string',
+      required: false
     },
     address: {
-        type: 'string',
-        required: false
+      type: 'string',
+      required: false
     },
     city: {
-        type: 'string',
-        required: false
+      type: 'string',
+      required: false
     },
     country: {
-        type: 'string',
-        required: false
+      type: 'string',
+      required: false
     },
     description: {
-        type: 'string',
-        required: false
+      type: 'string',
+      required: false
     },
     available: { type: 'boolean', required: true },
-    availableDays: [{
-        availableDay: {
-            type: Date,
-            default: null
-        },
-        from: {
-            type: 'string',
-            required: false
-        },
-        to: {
-            type: 'string',
-            required: false
-        }
-    }]
-}, { timestamps: true })
+    availableDays: {
+      startDate: {
+        type: Date,
+        default: null
+      },
+      endDate: {
+        type: Date,
+        default: null
+      }
+    }
+  },
+  { timestamps: true }
+)
 
 const UserProfile = mongoose.model('profile', userProfile)
 module.exports = UserProfile
